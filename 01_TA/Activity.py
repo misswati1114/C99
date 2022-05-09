a60b8c341df36674596136ac4b5b22084ba88117
misswati1114 <69621898+misswati1114@users.noreply.github.com>
2022-05-09 08:38:26 +0530

    Add files via upload

---
+++ 01_TA/Activity.py
@@ -0,0 +1,37 @@
+import os 
+
+#os.system("date")
+
+#os.mkdir('C:/Users/swati/Desktop/C99')
+
+#os.getcwd()
+
+#path = "C:/Users/swati/Desktop/Travel_round_the_world"
+#isExist = os.path.exists(path)
+#print(isExist)
+
+#path = "C:/Users/Swati/Desktop/C99/01_TA/Test.txt"
+#root_ext = os.path.splitext(path)
+
+#print("root part ",root_ext[0])
+#print("ext part ",root_ext[1],"\n")
+
+#os.listdir('C:/Users/swati/Desktop/documents/Work/Whitehat_jr/Module-3/99-Backup')
+
+
+import shutil
+
+
+#s = "C:/Users/swati/Desktop/documents/Work/Whitehat_jr/Module-3/99-Backup/01_Activity/01_TA/folder1/text1.txt"
+#d = "C:/Users/swati/Desktop/documents/Work/Whitehat_jr/Module-3/99-Backup/01_Activity/01_TA/folder2/text2.txt"
+#dest = shutil.copy(s,d)
+
+
+#s = "C:/Users/swati/Desktop/documents/Work/Whitehat_jr/Module-3/99-Backup/01_Activity/01_TA/folder1/text1.txt"
+#d = "C:/Users/swati/Desktop/documents/Work/Whitehat_jr/Module-3/99-Backup/01_Activity/01_TA/folder2/text2.txt"
+#dest = shutil.move(s,d)
+
+
+
+
+
